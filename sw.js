1f67b86798a86a621f5ff1f801399431c01cc225
Contato Certo SP New <contatocerto.prestadores@gmail.com>
2026-09-25 13:11:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+
+const CACHE_NAME = 'contato-certo-v3';
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/logo.jpg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
