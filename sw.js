7ef1ad99f1130c43c95834c1cf3685a8fcfcc6ea
Contato Certo SP New <contatocerto.prestadores@gmail.com>
2026-09-25 13:41:11 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,6 @@
 
-const CACHE_NAME = 'contato-certo-v3';
-const urlsToCache = ['/', '/index.html', '/manifest.json', '/logo.jpg'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response;
-      return fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
+const CACHE_NAME = 'contato-certo-v4-final';
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/logo.jpg', '/icon-192.png', '/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })))); });
